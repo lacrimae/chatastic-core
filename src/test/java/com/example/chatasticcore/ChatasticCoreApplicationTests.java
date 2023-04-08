d63b709cc805f0472fd1c1863148cbeb921b1307
Kseniia Lebedeva <lightgraywrite@gmail.com>
2023-04-07 18:02:05 -0700

    Init.

---
+++ src/test/java/com/example/chatasticcore/ChatasticCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatasticcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatasticCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
